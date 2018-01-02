2cf1a88f3dfa693017ce4f30e1eb7600368fb17a
Miguel <Samirmiguelvoillaume@gmail.com>
2018-01-02 14:34:08 +0100

    Update configMysql.js

--- configuration/configMysql.js
+++ configuration/configMysql.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     host     : 'localhost',    //lieu de la base de donne
     user     : 'root',         //nom d'utilisateur
     password : "",             //mot de passe
-    database : 'projetfred'    //nom de la base de donne
+    database : '"NameDB"'    //nom de la base de donne
 });
 
 module.exports = connection;
